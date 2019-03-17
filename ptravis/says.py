09c390d74813337fff4f4033611688bf05ab69f8
zhi <zhisays@outlook.com>
2019-03-17 13:12:02 +0800

    p travis

---
+++ ptravis/says.py
@@ -0,0 +1,5 @@
+def say_hello():
+    return 'hello'
+
+def say_helloworld():
+    return "helloworld"
---
+++ tests/test_says.py
@@ -0,0 +1,5 @@
+from ptravis import says
+
+
+def test_say_hello():
+    assert "hello" == says.say_hello()
